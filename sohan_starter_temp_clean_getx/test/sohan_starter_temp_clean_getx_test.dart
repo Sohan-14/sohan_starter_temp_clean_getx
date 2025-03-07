fd70e3ef2f9535c3c4c76b46146c9b7141d47808
Md. Zahidul Islam Shohan <106897273+Sohan-14@users.noreply.github.com>
2025-03-07 20:44:57 +0600

    Delete sohan_starter_temp_clean_getx directory

--- sohan_starter_temp_clean_getx/test/sohan_starter_temp_clean_getx_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:sohan_starter_temp_clean_getx/sohan_starter_temp_clean_getx.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
